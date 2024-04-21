874d97b2280c762d60d9b2c19302fce7166b8dca
Szymon <szymonprz123@gmail.com>
2024-04-21 20:48:39 +0200

    added flash messages to send post form :OOO

--- components/AddPostForm.tsx
+++ components/AddPostForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import {useState} from "react";
 import axios from "axios";
 import {useRouter} from "next/navigation";
 
@@ -12,29 +12,40 @@ const AddPostForm = (props: props) => {
   const router = useRouter()
   const [postTitle, setPostTitle] = useState<string>("")
   const [postContent, setPostContent] = useState<string>("")
+  const [err, setErr] = useState<string>("")
 
   const sendPost = () => {
-    if(postTitle.trim() === "" || postContent.trim() === ""){
+    if (postTitle.trim() === "" || postContent.trim() === "") {
+      setErr("bad data")
       return
     }
 
-    console.log(props.section)
     axios.post('/api/addPost', {
       postTitle: postTitle.trim(),
       postContent: postContent.trim(),
       section: props.section,
     }).then((e) => {
+      if (e.data.err != undefined) {
+        setErr(e.data.err)
+      } else {
+        setErr("")
+      }
       router.refresh()
     })
   }
 
   return (
-    <div className="bg-orange-400 p-4 rounded-[50px] border-slate-500 border-8 flex flex-col items-start pl-8 pr-8 gap-2 mb-3">
+    <div
+      className="bg-orange-400 p-4 rounded-[50px] border-slate-500 border-8 flex flex-col items-start pl-8 pr-8 gap-2 mb-3">
       <label htmlFor="title">Tytuł posta</label>
-      <input onChange={(e) => setPostTitle(e.target.value)} type="text" id="uName" name="uName" className="rounded-xl w-full bg-slate-300 pl-2 pr-2" placeholder="eg. Łutyt"/>
+      <input onChange={(e) => setPostTitle(e.target.value)} type="text" id="uName" name="uName"
+             className="rounded-xl w-full bg-slate-300 pl-2 pr-2" placeholder="eg. Łutyt"/>
       <label htmlFor="content">Treść posta</label>
-      <textarea onChange={(e) => setPostContent(e.target.value)} name="content" id="content" rows={2} className="rounded-xl w-full bg-slate-300 p-2" placeholder="eg. Zadupienie tuby"></textarea>
-      <input onClick={() => sendPost()} type="button" value="Wyślij post" className="pl-2 pr-2 p-1 bg-orange-600 hover:bg-orange-500 transition cursor-pointer rounded-xl" />
+      <textarea onChange={(e) => setPostContent(e.target.value)} name="content" id="content" rows={2}
+                className="rounded-xl w-full bg-slate-300 p-2" placeholder="eg. Zadupienie tuby"></textarea>
+      <p>{err}</p>
+      <input onClick={() => sendPost()} type="button" value="Wyślij post"
+             className="pl-2 pr-2 p-1 bg-orange-600 hover:bg-orange-500 transition cursor-pointer rounded-xl"/>
     </div>
   )
 }
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,16 +1,17 @@
-import { getPosts } from "@/lib/actions"
+import {getPosts} from "@/lib/actions"
 
-export default async function Posts({ pathname, page }: { pathname: string, page: number }) {
+export default async function Posts({pathname, page}: { pathname: string, page: number }) {
   const posts: any[] = await getPosts(page, pathname)
 
   return (
     <div className="flex flex-col gap-4">
       {
         posts.map((post, idx) => (
-          <div key={idx} className="bg-orange-400 p-4 rounded-[50px] border-slate-500 border-8 flex flex-col items-start pl-8 pr-8 gap-2">
+          <div key={idx}
+               className="bg-orange-400 p-4 rounded-[50px] border-slate-500 border-8 flex flex-col items-start pl-8 pr-8 gap-2">
             <p className="text-2xl font-semibold">{post.title}</p>
             <p className="text-sm">{post.author}</p>
-            <hr className="border-1 border-slate-500 w-full" />
+            <hr className="border-1 border-slate-500 w-full"/>
             <p className="text-xl">{post.content}</p>
             <p>{post.createdDate}</p>
           </div>
