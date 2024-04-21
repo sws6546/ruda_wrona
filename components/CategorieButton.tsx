90dd73e3a68da732118a0a97ca71730476c1e235
Szymon <szymonprz123@gmail.com>
2024-04-21 21:03:40 +0200

    change categorie button collor if there's them categorie in using

--- components/CategorieButton.tsx
+++ components/CategorieButton.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import Link from "next/link"
-import { usePathname } from "next/navigation"
+import {usePathname} from "next/navigation"
 
-export default function CategorieButton({ title, link }: { title: any, link: any }) {
+export default function CategorieButton({title, link}: { title: any, link: any }) {
   const Pathname = usePathname()
   return (
-    <Link href={link} className={`bg-orange-400 p-4 rounded-[50px] border-slate-500 border-8 hover:bg-orange-500`}>
+    <Link href={link}
+          className={`${Pathname == link ? "bg-orange-200" : "bg-orange-400"} transition
+           p-4 rounded-[50px] border-slate-500 border-8 hover:bg-orange-500`}>
       <p>{title}</p>
+
     </Link>
   )
 }
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,17 +1,17 @@
 "use server"
 
 import clientPromise from "./mongodb"
-import { hash, compare } from "bcryptjs"
-import { cookies } from "next/headers"
+import {hash, compare} from "bcryptjs"
+import {cookies} from "next/headers"
 import jwt from "jsonwebtoken"
-import { redirect } from "next/navigation"
+import {redirect} from "next/navigation"
 
 export async function getPosts(page: number, pathname: string) {
   const client = await clientPromise
   const db = client.db("ruda-wrona")
   const postsCollection = db.collection('posts')
 
-  return await postsCollection.find({ path: pathname }).sort({createdDate: -1}).skip(page - 1).limit(10).toArray()
+  return await postsCollection.find({path: pathname}).sort({createdDate: -1}).skip(page - 1).limit(10).toArray()
 }
 
 export async function getSections() {
@@ -27,16 +27,18 @@ export async function register(formData: FormData) {
   const db = client.db("ruda-wrona")
   const userCollection = db.collection('users')
 
-  const user = await userCollection.find({ username: formData.get("uName") }).toArray()
+  const user = await userCollection.find({username: formData.get("uName")}).toArray()
   if (JSON.parse(JSON.stringify(user)).length == 0) {
-    await userCollection.insertOne({ username: formData.get('uName'), password: await hash(formData.get("uPwd") as string, 10) })
+    await userCollection.insertOne({
+      username: formData.get('uName'),
+      password: await hash(formData.get("uPwd") as string, 10)
+    })
     cookies().delete("err")
-    cookies().set("jwt", jwt.sign({ username: formData.get("uName") }, process.env.JWT_TOKEN as string))
-  }
-  else {
+    cookies().set("jwt", jwt.sign({username: formData.get("uName")}, process.env.JWT_TOKEN as string))
+    redirect("/")
+  } else {
     cookies().set("err", "account already exist")
   }
-  redirect("/")
 }
 
 export async function logout() {
@@ -52,10 +54,10 @@ export async function login(formData: FormData) {
   const db = client.db("ruda-wrona")
   const userCollection = db.collection('users')
 
-  const user = await userCollection.findOne({ username: formData.get("uName") })
+  const user = await userCollection.findOne({username: formData.get("uName")})
   if (user != null) {
     if (await compare(formData.get("uPwd") as string, user.password)) {
-      cookies().set("jwt", jwt.sign({ username: formData.get("uName") }, process.env.JWT_TOKEN as string))
+      cookies().set("jwt", jwt.sign({username: formData.get("uName")}, process.env.JWT_TOKEN as string))
       cookies().delete("err")
       redirect("/")
     } else {
